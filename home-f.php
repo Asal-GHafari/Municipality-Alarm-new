5a9306c2ccb26bbdba2f69695e790ff8a59a30b1
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-06-13 11:42:42 +0330

    new

---
+++ home-f.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link type="text/css"  rel="stylesheet" href="css/style.css"/>
+</head>
+
+<body>
+	<div class="slider-big"></div>
+	<div class="content">
+		<div class="header">
+        	<div class="header-left">
+            	<a href="home-f.php">
+                	<img src="pic-home/mashhad.gif" />
+                </a>
+        	</div>
+            <div class="div-menu">
+                <ul>
+                    <li><a href="">صفحه اصلی</a></li>
+                    <li><a href="">ارتباط با ما</a></li>
+                    <li><a href="">ارتباط با ما</a></li>
+                    <li><a href="">ارتباط با ما</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="slider"></div>
+        <div class="mainpanel">
+        	 <div class="div1"></div>
+             <div class="div2">
+             	<div class="left2">
+                	<a href=""><span>تخصص های ما</span></a>
+                	<div id="expertise1"><p>ایده نو</p></div>
+                    <div id="expertise2"><p>چاپ اخطار</p></div>
+                    <div id="expertise3"><p>طراحی صفحات وب</p></div>
+                    <div id="expertise4"><p>اشغال معابر</p></div>
+                    <div id="expertise5"><p>پیشخوان</p></div>
+                </div>
+               <div class="rigth2">
+               	<a href="">
+               		<span>بیشتر بدانید</span>
+                    <img src="pic-home/savoir-plus.png" />
+               </a>
+               </div>
+               <div class="div2-text">jkgjkgkg</div>
+             </div>
+             <div class="div3"></div>
+        </div>
+        <div class="footer"></div>
+	</div>
+    <div class="footer-big"></div>
+</body>
+</html>
