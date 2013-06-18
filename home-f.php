0f3f0fdac7033484acda0c289de9dc1affca6471
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-06-18 20:27:18 +0330

    extent

--- home-f.php
+++ home-f.php
@@ -29,36 +29,36 @@
         	 <div class="div1"></div>
              <div class="div2">
              	<div class="left2">
-                	<a href=""><span>تخصص های ما</span></a>
-                    <div class="le"><img src="pic-home/1.jpg" /><span>ایده نو</span></div>
-                    <div class="le"><img src="pic-home/1.jpg" /><span>چاپ اخطار</span></div>
-                    <div class="le"><img src="pic-home/1.jpg" /><span>طراحی صفحات وب</span></div>
-                    <div class="le"><img src="pic-home/1.jpg" /><span>اشغال معابر</span></div>
-                    <div class="le"><img src="pic-home/1.jpg" /><span>پیشخوان</span></div>
-                   
-                    
-                	<!--<div id="expertise1"><p>ایده نو</p></div>
-                    <div id="expertise2"><p>چاپ اخطار</p></div>
-                    <div id="expertise3"><p>طراحی صفحات وب</p></div>
-                    <div id="expertise4"><p>اشغال معابر</p></div>
-                    <div id="expertise5"><p>پیشخوان</p></div>-->
+                	<a href=""><span>تخصص های <font style="text-shadow:#F00;text-transform:capitalize">ما</font></span></a>
+                    <div class="le"><span></span></div>
+                    <div class="le"><img src="pic-home/5-m.png" /><span>پیشخوان</span></div>
+                    <div class="le"><img src="pic-home/3-m.png" /><span>طراحی صفحات وب</span></div>
+                    <div class="le"><img src="pic-home/1-m.png" /><span>ایده نو</span></div>
+
                 </div>
                <div class="rigth2">
-               	<a href="">
+               	<a href="" class="a">
                		<span>بیشتر بدانید</span>
                     <img src="pic-home/savoir-plus.png" />
                </a>
                </div>
-               <div class="div2-text">jkgjkgkg</div>
+               <div class="div2-text">
+               	<p class="a">
+                    اید یک مفهوم یا یک اثر ذهنی است.</br>
+                    تفاوت بين ساختن و خلق كردن دقيقا" اين است: به چيزي كه ساخته شود فقط پس از ساخته شدن مي توان عشق ورزيد </br>
+                    اما چيزي كه خلق مي شود قبل از به وجود آمدن عشق ورزيدني است
+                </p>
+               </div>
              </div>
              <div class="div3">
              	<div class="div3-3">
+                	<a href=""><span>پروژه ها</span></a> 
                 	<div></div>
                     <div></div>
                     <div></div>
                     <div>
-                        <a href="">
-                            <span>بیشتر بدانید</span>
+                        <a href="" class="a">
+                            <span>مشاهده تمام پروژه ها</span>
                             <img src="pic-home/savoir-plus.png" />
                         </a>
                     </div>
