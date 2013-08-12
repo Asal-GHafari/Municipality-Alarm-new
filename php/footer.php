7b30921bc5d4938e563d7745ac780663daf35756
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-08-12 23:02:31 +0330

    in

--- php/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-    <div class="footer1"></div>
-    <div class="footer2">
-        <div></div>
-        <div></div>
-	</div>
-	<div class="footer3"></div>
-</div>
-
--- php/header.php
+++
@@ -1,8 +0,0 @@
-<div class="header">
-    <div class="header-left">
-        <a href="home-f.php">
-        	<img src="pic-home/mashhad.gif" />
-        </a>
-    </div>
-    <?php include("menu.php") ?>
-</div>
