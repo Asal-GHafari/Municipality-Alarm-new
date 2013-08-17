103fd3eddb03bed09ffd296ee126cb3a9f609e55
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-08-17 17:36:31 +0330

    ins

---
+++ php/slider/slider-home.php
@@ -0,0 +1,24 @@
+<h1>WEB DEVELOPER</h1>
+<table border="0">
+ <tr>
+ 	<td><label>name</label></td>
+    <td>Farzane GHafari</td>
+ </tr>
+  <tr>
+ 	<td><label>Birthday</label></td>
+    <td>March 30,1987</td>
+ </tr>
+  <tr>
+ 	<td><label>Address</label></td>
+    <td>Iran ,Mashhhad</td>
+ </tr>
+  <tr>
+ 	<td><label>Email</label></td>
+    <td>asal.ghafari2011@gmail.com</td>
+ </tr>
+  <tr>
+ 	<td><label>Website</label></td>
+    <td>farzane-ghafari.ir</td>
+ </tr>
+</table>
+<img src="pic-home/11.jpg"  id="phone1"/>
