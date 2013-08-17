6c127f7dfb7896b7df7efd76174359d046494d7e
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-08-17 17:35:32 +0330

    php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style type="text/css">
+	div{
+		width:178px;
+		height:184px;
+		position:absolute;
+		top:0;
+		bottom:0;
+		left:0;
+		right:0;
+		margin:auto;
+		background:url(pic-home/Snapshot%201.jpg) repeat 343px 470px;
+	}
+</style>
+</head>
+
+<body>
+<a href="home.php"> <div></div></a>
+</body>
+</html>
