760e13fbb70ace9f1f66aac953829a154c33dbe8
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-08-23 15:22:07 +0330

    I

--- php/header.php
+++
@@ -1,8 +0,0 @@
-<div class="header">
-    <div class="header-left">
-        <a href="home-f.php">
-        	<img src="https://lh5.googleusercontent.com/-Tdd_BOl_YXo/T4LAkzyTSsI/AAAAAAAAA18/JpyHedGEuAo/s140/community_twitter.png" />
-        </a>
-    </div>
-    <?php include("menu.php") ?>
-</div>
--- php/menu.php
+++
@@ -1,8 +0,0 @@
-<div class="div-menu">
-<ul>
-    <li><a href="home.php">صفحه اصلی</a></li>
-    <li><a href="takhasos.php">تخصص ما</a></li>
-    <li><a href="">مراجع</a></li>
-    <li><a href="contact.php">ارتباط با ما</a></li>
-</ul>
-</div>
